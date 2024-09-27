4e48704f185b2264620bf54240df723b695bbfc1
juanthomasw <104194668+juanthomasw@users.noreply.github.com>
2024-09-27 14:35:01 +0700

    Update train.py

--- train.py
+++ train.py
@@ -359,7 +359,8 @@ def train(hyp, opt, device, tb_writer=None):
             # Forward
             with amp.autocast(enabled=cuda):
                 pred = model(imgs)  # forward
-                    loss, loss_items = compute_loss(pred, targets.to(device))  # loss scaled by batch_size
+                loss, loss_items = compute_loss(pred, targets.to(device))  # loss scaled by batch_size
+                
                 if rank != -1:
                     loss *= opt.world_size  # gradient averaged between devices in DDP mode
                 if opt.quad:
