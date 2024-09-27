d261b4c68b3e13def5203e006e9d0652ca50b56d
juanthomasw <104194668+juanthomasw@users.noreply.github.com>
2024-09-27 11:52:38 +0700

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -594,10 +594,16 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
             img, ratio, pad = letterbox(img, shape, auto=False, scaleup=self.augment)
             shapes = (h0, w0), ((h / h0, w / w0), pad)  # for COCO mAP rescaling
 
-            labels = self.labels[index].copy()
-            if labels.size:  # normalized xywh to pixel xyxy format
-                labels[:, 1:5] = xywhn2xyxy(labels[:, 1:], ratio[0] * w, ratio[1] * h, padw=pad[0], padh=pad[1])
+            labels = []
+            x = self.labels[index].copy()
 
+            if x.size > 0:
+                labels = x.copy()
+                labels[:, 1] = w * x[:, 1]
+                labels[:, 2] = h * x[:, 2]
+                labels[:, 3] = w * x[:, 3]
+                labels[:, 4] = h * x[:, 4]
+                
         if self.augment:
             # Augment imagespace
             if not mosaic:
