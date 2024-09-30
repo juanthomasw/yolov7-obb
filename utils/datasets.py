940b54a91770757be5fd6a4fa51615b2118d3ca7
juanthomasw <104194668+juanthomasw@users.noreply.github.com>
2024-09-30 15:32:58 +0700

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -30,7 +30,7 @@ from torchvision.ops import roi_pool, roi_align, ps_roi_pool, ps_roi_align
 from utils.general import check_requirements, xyxy2xywh, xywh2xyxy, xywhn2xyxy, xyn2xy, segment2box, segments2boxes, \
     resample_segments, clean_str, rotation_boxes
 from utils.torch_utils import torch_distributed_zero_first
-from utils.rboxs_utils import gaussian_label_cpu
+from utils.rboxs_utils import gaussian_label_cpu, rbox2poly
 
 # Parameters
 help_url = 'https://github.com/ultralytics/yolov5/wiki/Train-Custom-Data'
