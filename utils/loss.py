21da2cb8bb89b8be9be0d4be944873b6e9efa0af
juanthomasw <104194668+juanthomasw@users.noreply.github.com>
2024-10-22 11:38:06 +0700

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -477,7 +477,7 @@ class ComputeLoss:
                 _, ptheta = torch.max(ps[:, 4:184], 1,  keepdim=True) # [n_conf_thres, 1] θ ∈ int[0, 179]
                 ptheta = (ptheta - 90) / 180 * math.pi # [n_conf_thres, 1] θ ∈ [-pi/2, pi/2 ]
                 
-                iou = bbox_iou_obb(pbox.T, tbox[i], ptheta, ttheta[i], x1y1x2y2=False, CIoU=True)  # iou(prediction, target)
+                iou = bbox_iou_obb(pbox.T, tbox[i], ptheta, ttheta[i], CIoU=True)  # iou(prediction, target)
                 lbox += (1.0 - iou).mean()  # iou loss
 
                 # Objectness
